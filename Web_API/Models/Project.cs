d75424dddbfceedd48c7455a9037d55101055c1d
Budkin Andrey <butyaga@rambler.ru>
2025-02-28 17:40:48 +0300

    Add ProjectsController

---
+++ Web_API/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using API_Abstract;
+
+namespace Web_API.Models;
+public class Project : IProject
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public IEnumerable<ITask> Tasks { get; set; } = [];
+}
