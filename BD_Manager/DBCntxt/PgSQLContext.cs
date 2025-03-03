e7a98457a5abdc0450dbb774cc716c0dbbe4e89e
Budkin Andrey <butyaga@rambler.ru>
2025-03-03 17:34:17 +0300

    Add dependencies to Web_API

--- BD_Manager/DBCntxt/PgSQLContext.cs
+++ BD_Manager/DBCntxt/PgSQLContext.cs
@@ -2,36 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace DB_Manager.DBCntxt;
-public class PgSQLContext : DbContext
+public class PgSQLContext(DbContextOptions<PgSQLContext> options) : DbContext(options)
 {
     public DbSet<Project> Projects { get; set; }
-
-    public PgSQLContext() : base()
-    {
-        //Database.EnsureCreated();
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=usersdb;Username=postgres;Password=qwe")
-            .UseAsyncSeeding((async (context, _, cancellationToken) =>
-            {
-                var addProject = async (string name, string description) =>
-                {
-                    Project? project = await context.Set<Project>().FirstOrDefaultAsync(p => p.Name == name, cancellationToken);
-                    if (project is null)
-                    {
-                        context.Set<Project>().Add(new Project { Name = name, Description = description });
-                        await context.SaveChangesAsync(cancellationToken);
-                    }
-                };
-
-                await addProject("Проект 1", "Описание проета 1");
-                await addProject("Проект 2", "Описание проета 2");
-                await addProject("Проект 3", "Описание проета 3");
-            }));
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Web_API/Program.cs
+++ Web_API/Program.cs
@@ -1,11 +1,19 @@
+using DB_Manager.DBCntxt;
+using DB_Manager.Managers;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
 namespace Web_API;
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        string connectionString = GetConnectionString(builder.Configuration);
+        builder.Services.AddDBManagers(connectionString);
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,6 +21,8 @@ public class Program
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
+
+        await app.MigrateDatabase();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -29,5 +39,13 @@ public class Program
         app.MapControllers();
 
         app.Run();
+    }
+
+    private static string GetConnectionString(ConfigurationManager configuration)
+    {
+        string? result = configuration["PGSQLCONNECTIONSTRING"];
+        result ??= configuration.GetConnectionString("PgSQLConnection");
+        result ??= "Host=localhost;Port=5433;Database=usersdb;Username=postgres;Password=qwe";
+        return result;
     }
 }
