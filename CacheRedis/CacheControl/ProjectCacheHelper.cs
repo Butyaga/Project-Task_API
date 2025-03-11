24143356e014e44a3c190579d6681b3067e5b283
Budkin Andrey <butyaga@rambler.ru>
2025-03-11 09:45:20 +0300

    Save minor changes

---
+++ CacheRedis/CacheControl/ProjectCacheHelper.cs
@@ -0,0 +1,15 @@
+﻿using API_Abstract.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheRedis.CacheControl;
+class ProjectCacheHelper
+{
+    async Task Save(IProjectDTO project)
+    {
+
+    }
+}
